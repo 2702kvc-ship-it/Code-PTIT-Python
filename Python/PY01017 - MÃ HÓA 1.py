da8ce59a19169743741747b2bfcd39f424fd8a42
2702kvc-ship-it <2702kvc@gmail.com>
2025-11-05 01:21:49 +0700

    Add files via upload

---
+++ Python/PY01017 - MÃ HÓA 1.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    s = input() + '@'
+    cnt = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            cnt += 1
+        else:
+            print(f"{cnt}{s[i - 1]}", end='')
+            cnt = 1
+    print()
