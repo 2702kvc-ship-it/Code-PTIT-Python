d77cf2334dd68f3e4e87f7ae722745d14d6b6d2c
LanGiaoSu <2702kvc@gmail.com>
2025-11-14 16:41:13 +0700

    Add files via upload

---
+++ Python/PY02032 - LIỆT KÊ CÁC SỐ CÓ HAI CHỮ SỐ TĂNG DẦN.py
@@ -0,0 +1,14 @@
+s = input()
+res = []
+if len(s) % 2 == 0:
+    for i in range(0, len(s), 2):
+        tmp = '' + s[i] + s[i + 1]
+        res.append(int(tmp))
+else:
+    for i in range(0, len(s) - 1, 2):
+        tmp = '' + s[i] + s[i + 1]
+        res.append(int(tmp))
+
+res = list(sorted(set(res)))
+
+print(*res)
